407053745d07a7c2b68484d363aaef9171cf7607
Justin Hong <jjhong922@berkeley.edu>
2021-08-12 18:32:14 -0700

    move install data to notebook

--- install.R
+++ install.R
@@ -5,9 +5,7 @@ install.packages("anndata")
 install.packages("devtools")
 
 devtools::install_github("satijalab/seurat-data")
-InstallData("pbmc3k")
 install.packages("https://seurat.nygenome.org/src/contrib/ifnb.SeuratData_3.0.0.tar.gz", repos = NULL, type = "source") 
-InstallData("ifnb")
 
 install.packages("BiocManager")
 BiocManager::install(c("LoomExperiment", "SingleCellExperiment"))
